c3682f00cf3ca162359a072725b5d87d57c991c7
Mohit <mophitbhatia612@gmail.com>
2024-03-19 16:03:44 -0400

    ...

--- src/Layout/Signin.js
+++ src/Layout/Signin.js
@@ -1,27 +1,29 @@
+import logo from "C:/Users/mohit/Desktop/TalkTrek/src/logo.svg"
+
 export default function Signin() {
     return (
         <>
             <div className="container">
-                <div className="logo">
-                    <h1>logo</h1>
-                </div>
+
+                <img src={logo} alt="TalkTrek" />
+
                 <form action="">
-                    <div className="text-sign">
-                        <h1>
-                            Sign in to start <br />
-                            messaging
-                        </h1>
-                    </div>
-                    <div className="emailbox-password">
-                        <div className="emailbox">
-                            <input type="email" placeholder="Enter E-mail or Phone . . ." />
-                        </div>
-                        <div className="password">
-                            <input type="password" placeholder="Enter Password . . ." />
-                        </div>
-                        <div className="submit-button">
-                            <button>Sign in</button>
-                        </div>
+
+                    <big>
+                        Sign in to start <br />
+                        messaging
+                    </big>
+
+                    <div className="login-fields">
+
+                        <label htmlFor="email">E-mail / phone</label>
+                        <input id="email" type="email" />
+
+                        <label htmlFor="pass">Password</label>
+                        <input id="pass" type="password" />
+
+                        <button>Sign in</button>
+
                     </div>
                 </form>
             </div>
